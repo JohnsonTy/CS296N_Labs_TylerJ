7b11fb132243081634479aa2d3b4e622965ef2e0
JohnsonTy <johnsontb@my.lanecc.edu>
2024-01-30 03:26:30 -0800

    Construct login page (discriminator issue)

--- PineappleFanSite/Controllers/RegisterController.cs
+++ PineappleFanSite/Controllers/RegisterController.cs
@@ -43,8 +43,35 @@ namespace PineappleFanSite.Controllers
                 }
             }
 
+
             // If we got this far, something failed, redisplay form
             return View(model);
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, true, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- PineappleFanSite/Data/AppDbContext.cs
+++ PineappleFanSite/Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace PineappleFanSite.Data
 {
@@ -12,7 +13,23 @@ namespace PineappleFanSite.Data
 
         public DbSet<Messages> Messages { get; set; }
         // TODO: Remove Users when we add Identity
-        public DbSet<AppUser> Users { get; set; }
+        public override DbSet<IdentityUser> Users { get; set; }
         public DbSet<Stories> Stories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityUser>()
+                .ToTable("AspNetUsers")
+                .HasNoDiscriminator();
+            
+            modelBuilder.Entity<AppUser>()
+                .ToTable("AspNetUsers")
+                .HasNoDiscriminator();
+
+            modelBuilder.Entity<Messages>()
+                .ToTable("Messages");
+        }
     }
 }
